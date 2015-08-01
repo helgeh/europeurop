e60b6338dd2f4c6afb42a83c080f2f2b4b54542a
Helge Hofstad <helge.hofstad@gmail.com>
2015-08-01 09:50:47 +0200

    snodig git-oppførsel her nå

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -2,6 +2,7 @@
 
 angular.module('europeuropApp')
   .config(function ($routeProvider) {
+    // NÅ BLIR JEG SNART SUR!
     $routeProvider
       .when('/', {
         templateUrl: 'app/main/main.html',
